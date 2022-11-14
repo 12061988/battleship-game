a6469b0ce2f02e9b602fd346d2a8fe8a2a046a16
12061988 <m.maguettefall@gmail.com>
2022-11-14 19:39:18 +0000

    add new changes to functions and the whole board, points and target

--- run.py
+++ run.py
@@ -1,89 +1,26 @@
 from random import randint
-"""
-Board for  ship position
-"""
-CACHE_BOARD = [[" "] * 8 for x in range(8)]
-"""
-Board for running points and missing targets
-"""
-IMAGINE_BOARD = [[" "] * 8  for i in range(8)]
 
-def print_board(board):
-    print("  A B C D E F G H ")
-    print(" +---------------+ ")
-    row_number = 1
-    for row in board:
-        print("%d|%s|" % (row_number, "|".join(row)))
-        row_number += 1
+def show_board(hit,miss,comp):
+    print("  Welcome to our battleships Game     ")
+    print("     0  1  2  3  4  5  6  7  8  9")
 
-letters_to_nums = {
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ " 
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch = " o "
+            elif place in comp:
+                ch = " 0 "
 
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3,
-    'E': 4,
-    'F': 5,
-    'G': 6,
-    'H': 7   
-    
-}
+            row = row + ch 
+            place = place + 1
+        print (x," ", row)     
 
-def create_ships(board):
-    """
-     5 ships creating by computer
-    """
-    for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
-        while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = get_ship_location()
-        board[ship_row][ship_column] = "X"
-
-def get_ship_location():
-    row = input("Enter the row of the ship:\n ").upper()
-    while row not in "12345678":
-        print('A selection of valid row is required')
-        row = input("Enter the row of the ship:\n ").upper()
-    column = input("Enter the column of the ship:\n ").upper()
-    while column not in "ABCDEFGH":
-        print('A correct column selection is required')
-        column = input("Enter the column of the ship: ").upper()
-    return int(row) - 1, letters_to_nums[column]
-
-
-def count_hit_ships(board):
-
-    """
-    validation of touched ships
-    """
-    count = 0
-    for row in board:
-        for column in row:
-            if column == "X":
-                count += 1
-    return count
-
-if __name__ == "__main__":
-    create_ships(CACHE_BOARD)
-    turns = 15
-    while turns > 0:
-        print('Welcome to our battleships competition\nTry to visualize a fight zone')
-        print_board(IMAGINE_BOARD)
-        row, column = get_ship_location()
-        if IMAGINE_BOARD[row][column] == "-":
-            print("You have already viewed it.")
-        elif CACHE_BOARD[row][column] == "X":
-            print("BRAVO")
-            IMAGINE_BOARD[row][column] = "X" 
-            turns -= 1  
-        else:
-            print("SORRY!")
-            IMAGINE_BOARD[row][column] = "-"   
-            turns -= 1     
-        if count_hit_ships(IMAGINE_BOARD) == 5:
-            print("Great! You got them!")
-            break
-        print("You have " + str(turns) + " turns left")
-        if turns == 0:
-            print("Game Over")
-
+hit = [21,22]
+miss = [20,24,12,13]
+comp = [23]
+show_board(hit,miss,comp)               
